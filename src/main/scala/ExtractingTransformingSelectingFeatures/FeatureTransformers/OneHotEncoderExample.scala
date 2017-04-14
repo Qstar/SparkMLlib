3f0f6eb23d5b39debd2eb91e61d73fe0d21eb2d9
qstar <davesla2012@hotmail.com>
2017-04-14 15:14:32 +0800

    update

---
+++ src/main/scala/ExtractingTransformingSelectingFeatures/FeatureTransformers/OneHotEncoderExample.scala
@@ -0,0 +1,37 @@
+package ExtractingTransformingSelectingFeatures.FeatureTransformers
+
+import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer}
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf, SparkContext}
+
+object OneHotEncoderExample {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("IndexToStringExample")
+    val sc = new SparkContext(conf)
+    val spark = new SQLContext(sc)
+
+    val df = spark.createDataFrame(Seq(
+      (0, "a"),
+      (1, "b"),
+      (2, "c"),
+      (3, "a"),
+      (4, "a"),
+      (5, "c")
+    )).toDF("id", "category")
+
+    val indexer = new StringIndexer()
+      .setInputCol("category")
+      .setOutputCol("categoryIndex")
+      .fit(df)
+    val indexed = indexer.transform(df)
+
+    val encoder = new OneHotEncoder()
+      .setInputCol("categoryIndex")
+      .setOutputCol("categoryVec")
+
+    val encoded = encoder.transform(indexed)
+    encoded.show(false)
+
+    sc.stop()
+  }
+}
