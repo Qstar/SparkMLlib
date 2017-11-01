76d5c3cddd9822f45bacb3153ab72cd52e47c43e
shuaiqiao <shuaiqiao@sohu-inc.com>
2017-11-01 17:25:48 +0800

    update

--- src/main/scala/MainGuide/ExtractingTransformingSelectingFeatures/FeatureExtractors/TfIdfExample.scala
+++ src/main/scala/MainGuide/ExtractingTransformingSelectingFeatures/FeatureExtractors/TfIdfExample.scala
@@ -1,11 +1,14 @@
 package MainGuide.ExtractingTransformingSelectingFeatures.FeatureExtractors
 
+import org.apache.log4j.{Level, Logger}
 import org.apache.spark.ml.feature.{HashingTF, IDF, Tokenizer}
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 object TfIdfExample {
   def main(args: Array[String]): Unit = {
+    //屏蔽日志
+    Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
     val conf = new SparkConf().setAppName("TfIdfExample")
     val sc = new SparkContext(conf)
     val spark = new SQLContext(sc)
@@ -13,16 +16,21 @@ object TfIdfExample {
     val sentenceData = spark.createDataFrame(Seq(
       (0.0, "Hi I heard about Spark"),
       (0.0, "I wish Java could use case classes"),
-      (1.0, "Logistic regression models are neat")
+      (1.0, "Logistic regression models are neat"),
+      (1.0, "I I I I I")
     )).toDF("label", "sentence")
 
+    println("=========tokenizedData================================================")
     val tokenizer = new Tokenizer().setInputCol("sentence").setOutputCol("words")
     val wordsData = tokenizer.transform(sentenceData)
+    wordsData.foreach(data => println(data))
 
     val hashingTF = new HashingTF().setInputCol("words").setOutputCol("rawFeatures").setNumFeatures(20)
 
+    println("=========featurizedData================================================")
     val featurizedData = hashingTF.transform(wordsData)
     // alternatively, CountVectorizer can also be used to get term frequency vectors
+    featurizedData.foreach(data => println(data))
 
     val idf = new IDF().setInputCol("rawFeatures").setOutputCol("features")
     val idfModel = idf.fit(featurizedData)
